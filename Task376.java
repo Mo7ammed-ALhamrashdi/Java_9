4c51f2290fb61ebfe81b7149bc8098f40dd0a49f
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 08:56:25 +0400

    Task376.java

---
+++ Task376.java
@@ -0,0 +1,21 @@
+package java_9;
+
+public class Task376 {
+    public class PrintNumbers {
+
+        public static void printUp(int n) {
+
+            if (n <= 0) {
+                return;
+            }
+
+            printUp(n - 1);
+
+            System.out.println(n);
+        }
+
+        public static void main(String[] args) {
+            printUp(5);
+        }
+    }
+}
