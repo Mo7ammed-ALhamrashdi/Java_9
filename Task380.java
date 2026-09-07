6923f52a1e6d8089e1fd31d0bb09d21811c88183
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 08:58:44 +0400

    Task380.java

---
+++ Task380.java
@@ -0,0 +1,19 @@
+package java_9;
+
+public class Task380 {
+    public class SumOfDigits {
+
+        public static int sumDigits(int number) {
+
+            if (number == 0) {
+                return 0;
+            }
+
+            return number % 10 + sumDigits(number / 10);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(sumDigits(4021));
+        }
+    }
+}
