b5751b467509daefde279aff175946ac475984ca
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 09:12:01 +0400

    Task10-393.java

---
+++ Task393.java
@@ -0,0 +1,29 @@
+package java_9;
+
+public class Task393 {
+    public class FastPower {
+
+        public static long power(int base, int exponent) {
+
+            if (exponent == 0) {
+                return 1;
+            }
+
+            long half = power(base, exponent / 2);
+
+            if (exponent % 2 == 0) {
+                return half * half;
+            }
+
+            return base * half * half;
+        }
+
+        public static void main(String[] args) {
+
+            System.out.println(power(2, 10));
+            System.out.println(power(3, 4));
+            System.out.println(power(5, 3));
+            System.out.println(power(2, 0));
+        }
+    }
+}
