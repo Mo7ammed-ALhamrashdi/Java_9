fbf393a97e14a9c5e91b4fe480299f20dfb61266
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 08:59:42 +0400

    Task381.java

---
+++ Task381.java
@@ -0,0 +1,28 @@
+package java_9;
+
+public class Task381 {
+    public class PowerOfTwo {
+
+        public static boolean isPowerOfTwo(int number) {
+
+            if (number == 1) {
+                return true;
+            }
+
+            if (number <= 0 || number % 2 != 0) {
+                return false;
+            }
+
+            return isPowerOfTwo(number / 2);
+        }
+
+        public static void main(String[] args) {
+
+            System.out.println(isPowerOfTwo(1));
+            System.out.println(isPowerOfTwo(8));
+            System.out.println(isPowerOfTwo(16));
+            System.out.println(isPowerOfTwo(6));
+            System.out.println(isPowerOfTwo(20));
+        }
+    }
+}
