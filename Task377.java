386b3274facb846ea15cc0cc28ca750079d9e45b
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 08:56:44 +0400

    Task377.java

--- Task377.java
+++ Task377.java
@@ -1,4 +1,19 @@
 package java_9;
 
 public class Task377 {
+    public class CountDigits {
+
+        public static int countDigits(int number) {
+
+            if (number < 10) {
+                return 1;
+            }
+
+            return 1 + countDigits(number / 10);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(countDigits(999999));
+        }
+    }
 }
