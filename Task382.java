96d3933ff4bd3e1d783122c7acd1189b7704be3e
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 09:01:09 +0400

    Task382.java

---
+++ Task382.java
@@ -0,0 +1,26 @@
+package java_9;
+
+public class Task382 {
+    public class RecursiveEven {
+
+        public static boolean isEven(int number) {
+
+            if (number == 0) {
+                return true;
+            }
+
+            if (number == 1) {
+                return false;
+            }
+
+            return isEven(number - 2);
+        }
+
+        public static void main(String[] args) {
+
+            System.out.println(isEven(8));
+            System.out.println(isEven(7));
+            System.out.println(isEven(100));
+        }
+    }
+}
