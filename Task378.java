145ca9367d2a45ffc8f9955b8ac7ebd7b3d8ad0f
MOHAMMED ALHAMRASHDI <malhamrashdi800@gmail.com>
2026-09-07 08:57:09 +0400

    Task378.java

--- Task378.java
+++ Task378.java
@@ -1,4 +1,19 @@
 package java_9;
 
 public class Task378 {
+    public class RecursiveMultiply {
+
+        public static int multiply(int a, int b) {
+
+            if (b == 0) {
+                return 0;
+            }
+
+            return a + multiply(a, b - 1);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(multiply(3, 4));
+        }
+    }
 }
